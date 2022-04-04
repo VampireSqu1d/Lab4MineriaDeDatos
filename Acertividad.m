273a22024c67a81584f296c3ff7cfbd65ca158e3
Cuauhtemoc Ayala Bautista <vampiresqu1d@tutanota.com>
2022-04-03 22:57:18 -0700

    Add files via upload

---
+++ Acertividad.m
@@ -0,0 +1,33 @@
+function [outputArg1] = Acertividad(Clusters, Sa, Ve, Vi)
+% En esta función se cálcula que cluster se adecúa más a que clase y se le
+% asigna, además se cálcula el porcentaje de acetividad.
+
+z = size(Clusters,1);
+Labels = zeros(z,1);
+acertividad = zeros(z,1);
+Sa(5) = [];
+Ve(5) = [];
+Vi(5) = [];
+
+for c=1: z
+    tempMean = mean(Clusters(c,:));
+    Labels(c, 1) = 1;
+    
+    aa = (tempMean/ mean(Sa)) *100;
+    ee = (tempMean/ mean(Ve)) *100;
+    ii = (tempMean/ mean(Vi)) *100;
+    acertividad(c, 1) = aa;
+    eee = abs(ee-100);
+    if eee < abs(acertividad(c, 1)-100)
+        Labels(c, 1) = 2;
+        acertividad(c, 1) = ee;
+    end
+    if abs(ii-100) < abs(acertividad(c, 1)-100)
+        Labels(c, 1) = 3;
+        acertividad(c, 1) = ii;
+    end
+end
+
+outputArg1 = [Clusters Labels acertividad];
+end
+
--- Lab4Main.m
+++ Lab4Main.m
@@ -21,25 +21,57 @@ Clusters = GenerarPosicionClusters(k, irisTrainx10) / 10;
 grosor = 10;
 tipoLinea = ".";
 %gráfica de los tamaños de los pépalos
-% figure(1)
-% hold on
-% plot(SetosaDatos(:,3),SetosaDatos(:,4), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
-% plot(VersicolourDatos(:,3),VersicolourDatos(:,4), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
-% plot(VirginicaDatos(:,3),VirginicaDatos(:,4), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
-% plot(Clusters(:,3),Clusters(:,4), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
-% title("Pépalos");
-% hold off
-% %gráfica de los tamaños de los sépalos
-% figure(2)
-% hold on
-% plot(SetosaDatos(:,1),SetosaDatos(:,2), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
-% plot(VersicolourDatos(:,1),VersicolourDatos(:,2), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
-% plot(VirginicaDatos(:,1),VirginicaDatos(:,2), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
-% plot(Clusters(:,1),Clusters(:,2), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
-% title("Sépalos");
-% hold off
+figure(1)
+hold on
+plot(SetosaDatos(:,3),SetosaDatos(:,4), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
+plot(VersicolourDatos(:,3),VersicolourDatos(:,4), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
+plot(VirginicaDatos(:,3),VirginicaDatos(:,4), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+plot(Clusters(:,3),Clusters(:,4), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+title("Pépalos");
+hold off
+%gráfica de los tamaños de los sépalos
+figure(2)
+hold on
+plot(SetosaDatos(:,1),SetosaDatos(:,2), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
+plot(VersicolourDatos(:,1),VersicolourDatos(:,2), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
+plot(VirginicaDatos(:,1),VirginicaDatos(:,2), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+plot(Clusters(:,1),Clusters(:,2), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+title("Sépalos");
+hold off
 
 for c=1:k
     ClusterLabels = asignarclusters(irisTrain,Clusters);
     Clusters = RecalcularCentroides(irisTrain,ClusterLabels);
 end
+
+%gráficas de los clusters despúes de aplicar el algóritmo k-means
+%gráfica de los tamaños de los pépalos
+figure(3)
+hold on
+plot(SetosaDatos(:,3),SetosaDatos(:,4), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
+plot(VersicolourDatos(:,3),VersicolourDatos(:,4), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
+plot(VirginicaDatos(:,3),VirginicaDatos(:,4), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+plot(Clusters(:,3),Clusters(:,4), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+title("Pépalos");
+hold off
+%gráfica de los tamaños de los sépalos
+figure(4)
+hold on
+plot(SetosaDatos(:,1),SetosaDatos(:,2), tipoLinea, 'color', 'blue', 'MarkerFaceColor', 'blue', 'MarkerSize', grosor);
+plot(VersicolourDatos(:,1),VersicolourDatos(:,2), tipoLinea, 'color', 'red', 'MarkerFaceColor', 'red', 'MarkerSize', grosor);
+plot(VirginicaDatos(:,1),VirginicaDatos(:,2), tipoLinea, 'color', '#7E2F8E', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+plot(Clusters(:,1),Clusters(:,2), tipoLinea, 'color', 'green', 'MarkerFaceColor', '#7E2F8E', 'MarkerSize', grosor);
+title("Sépalos");
+hold off
+
+ClustersLabeled = Acertividad(Clusters, Sa, Ve, Vi);
+
+
+
+
+
+
+
+
+
+
